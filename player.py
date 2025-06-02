dd1a23aa6c443d93deb5ad745a2fb82bd66b661e
enzopavani <tijoloenso@gmail.com>
2025-06-01 23:26:09 -0300

    Player animation

--- player.py
+++ player.py
@@ -3,6 +3,8 @@ from settings import *
 class Player(pygame.sprite.Sprite):
   def __init__(self, groups, position, collisionSprites):
     super().__init__(groups)
+    self.loadImages()
+    self.state, self.frameIndex = "down", 0
     self.image = pygame.image.load(join("resources", "images", "player", "down", "0.png")).convert_alpha()
     self.rect = self.image.get_frect(center=position)
     self.hitboxRect = self.rect.inflate(-60, -90)
@@ -11,6 +13,17 @@ class Player(pygame.sprite.Sprite):
     self.direction = pygame.Vector2()
     self.speed = 700
     self.collisionSprites = collisionSprites
+
+  def loadImages(self):
+    self.frames = {"down": [], "left": [], "right": [], "up": []}
+
+    for state in self.frames.keys():
+      for folderPath, subFolders, fileNames in walk(join("resources", "images", "player", state)):
+        if fileNames:
+          for fileName in sorted(fileNames, key = lambda name: int(name.split(".")[0])):
+            fullPath = join(folderPath, fileName)
+            surface = pygame.image.load(fullPath).convert_alpha()
+            self.frames[state].append(surface)
 
   def input(self):
     keys = pygame.key.get_pressed()
@@ -35,6 +48,16 @@ class Player(pygame.sprite.Sprite):
           if self.direction.y > 0: self.hitboxRect.bottom = sprite.rect.top
           if self.direction.y < 0: self.hitboxRect.top = sprite.rect.bottom
 
+  def animate(self, dt):
+    if self.direction.x != 0:
+      self.state = "right" if self.direction.x > 0 else "left"
+    if self.direction.y != 0:
+      self.state = "down" if self.direction.y > 0 else "up"
+    
+    self.frameIndex = self.frameIndex + 10 * dt if self.direction else 0
+    self.image = self.frames[self.state][int(self.frameIndex) % len(self.frames[self.state])]
+
   def update(self, dt):
     self.input()
     self.move(dt)
+    self.animate(dt)
